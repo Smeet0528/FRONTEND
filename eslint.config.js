cc23867b1d839e498c37f153564a20ef3ca9f17b
somin2352 <somin030502@gmail.com>
2025-07-03 22:18:47 +0900

    Chore: eslint, prettier 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,51 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
 
 export default tseslint.config([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    settings: {
+      react: {
+        version: '19.0.0',
+      },
+    },
+    parserOptions: {
+      project: ['./tsconfig.app.json', './tsconfig.node.json'],
+      tsconfigRootDir: import.meta.dirname,
+      ecmaFeatures: { jsx: true },
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: { ...globals.browser, ...globals.node },
+    },
+    rules: {
+      ...react.configs.flat.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
     },
   },
-])
+]);
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,3 @@
 export default function App() {
-  return <div className='bg-amber-500'>Main Page</div>;
+  return <div className="bg-amber-500 text-green-800">Main Page</div>;
 }
