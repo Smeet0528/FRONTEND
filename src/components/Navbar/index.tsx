d631215881d3e95d17c421789ca63b59bf2d0727
somin2352 <somin030502@gmail.com>
2025-07-13 17:39:14 +0900

    Modify: 컴포넌트 파일명 변경에 따른 경로 변경

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,4 @@
-import Navlink from './link';
+import Navlink from './Link';
 import Home from '/src/assets/home.svg?react';
 import Study from '/src/assets/study.svg?react';
 
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav className="fixed bottom-0 w-full max-w-[480px] flex justify-between p-2 pb-3 bg-[#F8F8F8] z-50">
       <Navlink to="/" Icon={Home} alt="홈" />
       <div className="border border-[#c4c4c4]"></div>
-      <Navlink to="/study" Icon={Study} alt="스터디" />
+      <Navlink to="/study-list" Icon={Study} alt="스터디" />
     </nav>
   );
 };
