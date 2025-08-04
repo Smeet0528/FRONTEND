050fb9b01786d177877e1cf990026a83b2fc4795
somin2352 <somin030502@gmail.com>
2025-08-04 22:13:32 +0900

    chore: eslint 수정
    - api 호출 시 data type error off

--- eslint.config.js
+++ eslint.config.js
@@ -54,6 +54,7 @@ export default tseslint.config([
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'error',
+      '@typescript-eslint/no-unsafe-return': 'off',
     },
   },
 ]);
