b9930bd899e59aa668e3ead2290533d061485120
Jaeyeon-23 <reenact115@gmail.com>
2026-03-31 15:43:08 +0900

    [Feat]모임신청 api호출 추가

--- src/api/group.ts
+++ src/api/group.ts
@@ -6,6 +6,11 @@ import type {
   ResponseCreateGroupDto,
 } from '@/types/group';
 
+import type {
+  RequestApplyGroupDto,
+  ResponseApplyGroupDto,
+} from '@/types/group';
+
 //모임생성 api
 export const createGroup = async (
   body: RequestCreateGroupDto
@@ -19,5 +24,14 @@ export const getGroupDetail = async (
   id: string
 ): Promise<GroupDetailResponse> => {
   const { data } = await axiosInstance.get(`/groups/${id}`);
+  return data;
+};
+
+//모임 신청 API
+export const applyGroup = async (
+  id: string,
+  body: RequestApplyGroupDto
+): Promise<ResponseApplyGroupDto> => {
+  const { data } = await axiosInstance.post(`/applications/${id}`, body);
   return data;
 };
